8a1e233f96794fa6ff1b857bf04fd10779b6eef8
Ali X <al00x@outlook.com>
2023-11-06 10:37:15 +0330

    Updated playground

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {AlxPrintDirective} from "al00x/printer";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('printer2') printer2!: AlxPrintDirective;
+
   title = 'playground';
 
-  customPrintFn = () => console.log('Print?');
+  customPrintFn = () => {
+    console.log('Print?');
+    setTimeout(() => {
+      this.printer2.cleanup();
+    }, 1000)
+  };
 }
